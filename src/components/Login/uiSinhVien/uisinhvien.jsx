c191e5feadcb8b456c01da4cc058c55677e352b6
Trong Tran <trong.tran@sstechvn.com>
2023-11-05 00:38:28 +0700

    hethon

--- src/components/Login/uiSinhVien/uisinhvien.jsx
+++ src/components/Login/uiSinhVien/uisinhvien.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Uisinhvien() {
   return (
     <div className='flex' >
-       <p className='bg-blend-color-dodge'></p>
+       <p className='bg-blend-color-dodge'> jhjj</p>
     </div>
   )
 }
